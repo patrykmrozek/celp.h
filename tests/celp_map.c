7c64b0e4b69d24851f4b3d54cc3b14554f39388b
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2026-01-10 17:19:50 +0000

    plan functionality to add to the hashmap

--- tests/celp_map.c
+++ tests/celp_map.c
@@ -12,6 +12,14 @@ int main() {
     celp_map_init(&test);
     celp_map_info(&test);
 
+    for (uint32_t i = 0; i < 10; i++) {
+        celp_map_add(&test, i);
+    }
+    celp_map_info(&test);
+    celp_map_add(&test, 5);
+    celp_map_get(&test, 5);
+
+    celp_map_free(&test);
 
     return 0;
 }
