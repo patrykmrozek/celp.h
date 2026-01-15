02dbe9c330bca71f588899202256903570785d54
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2026-01-15 09:38:04 +0000

    test vec sub

--- tests/celp_math.c
+++ tests/celp_math.c
@@ -13,6 +13,13 @@ int main() {
         "[%i %i] + [%i %i] = [%i %i]",
         w.x, w.y, e.x, e.y, r.x, r.y);
 
+    Vec2i w2 = {2, 2};
+    Vec2i e2 = {3, 3};
+    Vec2i r2 = celp_vec2_sub(w2, e2);
+    celp_log(CELP_LOG_LEVEL_DEBUG,
+        "[%i %i] - [%i %i] = [%i %i]",
+        w2.x, w2.y, e2.x, e2.y, r2.x, r2.y);
+
     Vec3i a = {1, 1, 1};
     Vec3i b = {1, 1, 1};
     Vec3i res = celp_vec3_add(a, b);
@@ -20,4 +27,12 @@ int main() {
     celp_log(CELP_LOG_LEVEL_DEBUG,
         "[%i %i %i] + [%i %i %i] = [%i %i %i]",
         a.x, a.y, a.z, b.x, b.y, b.z, res.x, res.y, res.z);
-}
+
+    Vec3i a2 = {4, 4, 4};
+    Vec3i b2 = {1, 1, 1};
+    Vec3i res2 = celp_vec3_sub(a2, b2);
+
+    celp_log(CELP_LOG_LEVEL_DEBUG,
+        "[%i %i %i] - [%i %i %i] = [%i %i %i]",
+        a2.x, a2.y, a2.z, b2.x, b2.y, b2.z, res2.x, res2.y, res2.z);
+    }
