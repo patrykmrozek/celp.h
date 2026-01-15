5f3d55d0a2b69d3c93e260b28227f8fc4ac13210
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2026-01-15 10:15:15 +0000

    test dot

--- tests/celp_math.c
+++ tests/celp_math.c
@@ -20,6 +20,11 @@ int main() {
         "[%i %i] - [%i %i] = [%i %i]",
         w2.x, w2.y, e2.x, e2.y, r2.x, r2.y);
 
+    int r3 = celp_vec2_dot(w, e);
+    celp_log(CELP_LOG_LEVEL_DEBUG,
+        "[%i %i] . [%i %i] = %i",
+        w.x, w.y, e.x, e.y, r3);
+
     Vec3i a = {1, 1, 1};
     Vec3i b = {1, 1, 1};
     Vec3i res = celp_vec3_add(a, b);
@@ -35,4 +40,10 @@ int main() {
     celp_log(CELP_LOG_LEVEL_DEBUG,
         "[%i %i %i] - [%i %i %i] = [%i %i %i]",
         a2.x, a2.y, a2.z, b2.x, b2.y, b2.z, res2.x, res2.y, res2.z);
-    }
+
+    int res3 = celp_vec3_dot(a2, b2);
+
+    celp_log(CELP_LOG_LEVEL_DEBUG,
+        "[%i %i %i] . [%i %i %i] = %i",
+        a2.x, a2.y, a2.z, b2.x, b2.y, b2.z, res3);
+}
