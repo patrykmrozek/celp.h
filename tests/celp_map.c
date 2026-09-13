ab67b9de064ae0d10d44057cde1a3f4dcf754a7a
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2026-09-14 01:58:56 +0800

    update map tests with new errors

--- tests/celp_map.c
+++ tests/celp_map.c
@@ -3,6 +3,7 @@
 
 celp_map(int, int);
 celp_map_t(int, int) map = {};
+celp_err_t error = CELP_ERR_OK;
 
 CELP_TEST_SETUP(map)
 {
@@ -25,57 +26,78 @@ CELP_TESTCASE(map_insert)
 {
     int amount = 10;
     for (int i = 0; i < amount; i++) {
-        celp_map_insert(&map, i, 100);
+        error = CELP_ERR_OK;
+        celp_map_insert(&map, i, 100, &error);
+        CELP_EXPECT_EQ(error, CELP_ERR_OK);
     }
     CELP_EXPECT_EQ(map.count, amount);
 }
 
 CELP_TESTCASE(map_contains)
 {
+    bool found;
     for (int i = 0; i < map.count; i++) {
-        CELP_EXPECT(celp_map_contains(&map, i));
+        error = CELP_ERR_OK;
+        found = celp_map_contains(&map, i, &error);
+        CELP_EXPECT_EQ(error, CELP_ERR_OK);
+        CELP_EXPECT(found);
     }
-    CELP_EXPECT(!celp_map_contains(&map, 1000));
+    CELP_EXPECT(!celp_map_contains(&map, 1000, &error));
 }
 
 CELP_TESTCASE(map_get)
 {
-    int ret = celp_map_get(&map, 5, -1);
+    error = CELP_ERR_OK;
+    int ret = celp_map_get(&map, 5, -1, &error);
+    CELP_EXPECT_EQ(error, CELP_ERR_OK);
+    CELP_EXPECT_NEQ(ret, -1);
     CELP_EXPECT_EQ(ret, 100);
 
-    ret = celp_map_get(&map, 999, -1);
+    ret = celp_map_get(&map, 999, -1, &error);
+    CELP_EXPECT_EQ(error, CELP_ERR_OK);
     CELP_EXPECT_EQ(ret, -1);
 }
 
 CELP_TESTCASE(map_increment_existing_value)
 {
-    int before = celp_map_get(&map, 5, -1);
+    error = CELP_ERR_OK;
+    int before = celp_map_get(&map, 5, -1, &error);
+    CELP_EXPECT_EQ(error, CELP_ERR_OK);
     CELP_EXPECT_NEQ(before, -1);
 
-    celp_map_increment(&map, 5);
-    int after = celp_map_get(&map, 5, -1);
+    celp_map_increment(&map, 5, &error);
+    int after = celp_map_get(&map, 5, -1, &error);
+    CELP_EXPECT_EQ(error, CELP_ERR_OK);
     CELP_EXPECT_EQ(after, before+1);
 }
 
 CELP_TESTCASE(map_increment_non_existing_value)
 {
+    error = CELP_ERR_OK; 
     int key = 999;
-    int before = celp_map_get(&map, key, -1);
+    int before = celp_map_get(&map, key, -1, &error);
+    CELP_EXPECT_EQ(error, CELP_ERR_OK);
     CELP_EXPECT_EQ(before, -1);
 
-    celp_map_increment(&map, key);
-    int after = celp_map_get(&map, key, -1);
+    celp_map_increment(&map, key, &error);
+    CELP_EXPECT_EQ(error, CELP_ERR_OK);
+
+    int after = celp_map_get(&map, key, -1, &error);
+    CELP_EXPECT_EQ(error, CELP_ERR_OK);
     CELP_EXPECT_NEQ(after, -1);
     CELP_EXPECT_EQ(after, 1);
 }
 
 CELP_TESTCASE(map_remove)
 {
-    int ret = celp_map_remove(&map, 999, -1);
+    error = CELP_ERR_OK;
+    int ret = celp_map_remove(&map, 999, &error);
     CELP_EXPECT_NEQ(ret, -1);
+    CELP_EXPECT_EQ(error, CELP_ERR_OK);
 
-    ret = celp_map_remove(&map, 999, -1);
-    CELP_EXPECT_EQ(ret, -1);
+    ret = celp_map_remove(&map, 999, &error);
+    CELP_EXPECT_EQ(error, CELP_ERR_OK);
+    CELP_EXPECT_EQ(ret, 0);
 }
 
 CELP_TEST_SUITE_START(kv_map);
